dd02f83e9b94cbbb0ed3e95f76fb414fca39a664
SlyVenegas <venegasly@gmail.com>
2024-12-20 12:46:05 -0500

    Carga inicial del proyecto

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import arrow_icon from "../../assets/arrow_icon.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <a href="/">
+        <img src={logo} alt="" className="logo" />
+      </a>
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="cop">COP</option>
+        </select>
+        <button>
+          Sign up <img src={arrow_icon} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
